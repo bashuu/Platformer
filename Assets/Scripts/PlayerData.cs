d577e0cd4f15914ab53e5aca40f67c3c6bdb8d4d
Sumyakhuu <sumyahuu@gmail.com>
2020-07-20 12:09:30 +0800

    added data holder

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [CreateAssetMenu (fileName = "PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public float moveSpeed = 6f;
+    public float jumpHeight = 5f;
+    public float dashDis = 3f;
+    public int lastMoveDir = 1;
+    public int MaxAirJump = 3;
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -8,22 +8,15 @@ public class PlayerInput : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    public float moveSpeed = 6f;
-    public float jumpHeight = 5f;
-    public float fallMult = -2f;
-    public float dashDis = 3f;
-
-    [SerializeField] public LayerMask platformLayerMask;
-
-    private const int MaxAirJump = 3;
 
     private bool isJumpPressed;
     private bool isDashPressed;
     private bool isCtrlPressed;
     private int jumpCount;
-
-    public Vector2 movement;
-    private int lastMoveDir = 1;
+    [SerializeField] 
+    private PlayerData playerData;
+    public LayerMask platformLayerMask;
+    public Vector2 movement;    
 
     private void Awake()
     {
@@ -33,7 +26,7 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        changeDir(lastMoveDir);
+        changeDir(playerData.lastMoveDir);
 
         handleMovementInput();
 
@@ -43,9 +36,9 @@ public class PlayerInput : MonoBehaviour
     {
         handleWalljump();
         handleJump();
-        handleDash(lastMoveDir);
+        handleDash(playerData.lastMoveDir);
 
-        rb.velocity = movement * moveSpeed + new Vector2(0.0f, rb.velocity.y);
+        rb.velocity = movement * playerData.moveSpeed + new Vector2(0.0f, rb.velocity.y);
     }
 
     bool isGrounded()
@@ -64,12 +57,12 @@ public class PlayerInput : MonoBehaviour
 
     void dash(float distance)
     {
-        transform.position += new Vector3(lastMoveDir, 0, 0) * distance;
+        transform.position += new Vector3(playerData.lastMoveDir, 0, 0) * distance;
     }
 
     void jump()
     {
-        rb.velocity = Vector2.up * jumpHeight;
+        rb.velocity = Vector2.up * playerData.jumpHeight;
         jumpCount--;
         
     }
@@ -79,14 +72,14 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             movement.x = -1;
-            lastMoveDir = -1;
+            playerData.lastMoveDir = -1;
         }
         else
         {
             if (Input.GetKey(KeyCode.D))
             {
                 movement.x = 1;
-                lastMoveDir = 1;
+                playerData.lastMoveDir = 1;
             }
             else
             {
@@ -111,16 +104,11 @@ public class PlayerInput : MonoBehaviour
     {
 
         if(isGrounded())
-            jumpCount = MaxAirJump;
+            jumpCount = playerData.MaxAirJump;
         if (isJumpPressed && jumpCount > 0)
         {
             jump();
         }
-        /*if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime;
-        }
-        */
     }
     private void handleWalljump()
     {
@@ -144,14 +132,14 @@ public class PlayerInput : MonoBehaviour
         
         if (isDashPressed)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(dir, 0), dashDis, platformLayerMask);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(dir, 0), playerData.dashDis, platformLayerMask);
             if (hit.collider != null)
             {
                 float distance = Mathf.Abs(hit.point.x - transform.position.x);
                 dash(distance - 0.2f);
                 return;
             }            
-            dash(dashDis);
+            dash(playerData.dashDis);
 
         }
 
