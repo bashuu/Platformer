649a79ee8962e8b89f85ca1363075df0cd7a5470
Sumyakhuu <sumyahuu@gmail.com>
2020-11-11 20:18:20 +0800

    last commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        if(player != null)
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
---
+++ Assets/Scripts/EnemyScipts/EnemyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State
+    {
+        patroling
+    }
+
+    public bool checkGround;
+
+    private void Awake()
+    {
+        state = State.patroling;
+        checkGround = transform.Find("GroundCheck").GetComponent<GroundCheck>();
+    }
+
+    private State state;
+    private void Update()
+    {
+        switch (state){
+            default:
+            case State.patroling:
+                if (checkGround.isGrounded)
+                {
+
+                }
+                break;
+        }
+        
+    }
+}
